8ab9e99f2c5b0683d12a7dd8ec4b399421c29b02
Samuelsos007 <samueldasilvaoliveira17@gmail.com>
2022-06-09 15:58:51 -0300

    sintaxe de operadores

--- sintaxe de operadores/script.js
+++ sintaxe de operadores/script.js
@@ -19,9 +19,9 @@ let num2 = Number(n2.value)
         res.innerHTML += `,${num1} + ${num2} = ${n3}, ${n3} é menor que 10,\n`
     }if(n3 <20){
         res.innerHTML += `,${num1} + ${num2} = ${n3}, ${n3} é menor que 20,\n`
-    }else if(n3 >20){
-        res.innerHTML += `,${num1} + ${num2} = ${n3}, ${n3} é maior que 20,\n`
-    }else(n3 == 20);{
+    }else if(n3 == 20){
         res.innerHTML += `,${num1} + ${num2} = ${n3}, ${n3} é igual a 20,\n`
+    }else{
+        res.innerHTML += `,${num1} + ${num2} = ${n3}, ${n3} é maior que 20,\n`
     }
 }
