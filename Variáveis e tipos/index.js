c51ed7b923e46f28f7259ebbd60f53ed13d9984c
Samuelsos007 <samueldasilvaoliveira17@gmail.com>
2022-06-10 13:38:56 -0300

    Variáveis e Tipos

--- Variáveis e tipos/index.js
+++ Variáveis e tipos/index.js
@@ -17,3 +17,14 @@ if(firstname === 'joão'){
 //não apareceu o let pois ela é uma variavel de escopo local e var é uma de escopo global
 console.log(firstname, lastname);*/
 //Ultilize o let em vezes de var, por conta de segurança
+
+//const FIRIST_NAME = 'Stephany';
+
+//FIRIST_NAME = 'julia' você não reatribuir um valor de uma constante
+//const FIRIST_NAME = 'julia'; você redeclarar o valor de uma constante
+
+/*FIRIST_NAME = 'Stephany';
+
+console.log(FIRIST_NAME);
+a variavel precisa ser declarada antes da atribuição dos valores
+const FIRIST_NAME;*/
