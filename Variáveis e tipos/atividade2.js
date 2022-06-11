3006e21ff75a1ceb7980b797a470250e5e9f3b3f
Samuelsos007 <samueldasilvaoliveira17@gmail.com>
2022-06-11 17:52:32 -0300

    Variáveis e Tipos

--- Variáveis e tipos/atividade2.js
+++ Variáveis e tipos/atividade2.js
@@ -1,3 +1,19 @@
+function sub1(array1){
+    if(!array1) return -1;
+    if(!array1.length) return -1;
+    for(let i = 0; i < array1.length; i++){
+        if(array1[i] === 0){
+            console.log("Você já é zero!");
+        }else if(array1[i] % 2 === 0){
+            console.log(`Substituindo ${array1[i]} por 0...`)
+            array1[i] = 0;
+        }
+    }
+    return array1;
+}
+let arr = [1, 3, 4, 5, 80, 33, 76, 65 ,32]
+console.log(sub1(arr));
+//solução 2
 function sub(array){
     if (!array.length) return console.log(-1);
     const naozero = (num) => num !== 0;
