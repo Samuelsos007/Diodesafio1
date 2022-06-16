a13be1eea3723b02cac6917611f0ac98132f3881
Samuelsos007 <samueldasilvaoliveira17@gmail.com>
2022-06-16 16:36:08 -0300

    Debugging e Erro Handling

---
+++ Debugging e Error Handling/index.js
@@ -0,0 +1,29 @@
+function validaarry(arr, num){
+    try {
+        if(!arr && !num) throw new ReferenceError("Envie os parâmetros");
+
+    if(typeof arr !== 'Object') throw new TypeError("Array precisa ser do tipo object");
+    
+    if(typeof arr !== 'numb') throw new TypeError("Num precisa ser do tipo number");
+
+    if(arr.length !== num) throw new RangeError('Tamanho inválido!');
+
+    return arr;    
+    } catch (e) {
+        if(e instanceof ReferenceError){
+            console.log("Este erro é um ReferenceError!")
+            console.log(e.message)
+        }else if(e instanceof TypeError){
+            console.log("Este erro é um TypeError!")
+            console.log(e.message)
+        }else if(e instanceof RangeError){
+            console.log("Este erro é um RangeError!")
+            console.log(e.message)
+        }else {
+            console.log("Tipo de erro não esperado:"+ e)
+        }
+    }
+
+}
+
+console.log(validaarry([], 5));
